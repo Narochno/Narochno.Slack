91c602d5a0f92abd4a718415fc70c7ae33e7a0eb
Alan Edwardes <alan@alanedwardes.com>
2016-12-30 16:55:08 +0000

    Updates packages, switches to use new enum string parser

--- src/Narochno.Slack/Entities/SlackCode.cs
+++ src/Narochno.Slack/Entities/SlackCode.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.Serialization;
+﻿using Narochno.Primitives.Parsing;
 
 namespace Narochno.Slack.Entities
 {
@@ -7,36 +7,36 @@ namespace Narochno.Slack.Entities
         /// <summary>
         /// Message was sent successfully.
         /// </summary>
-        [EnumMember(Value = "ok")]
+        [EnumString("ok")]
         Ok,
         /// <summary>
         /// Typically indicates that received request is malformed — perhaps the JSON is structured incorrectly,
         /// or the message text is not properly escaped. The request should not be retried without correction.
         /// </summary>
-        [EnumMember(Value = "invalid_payload")]
+        [EnumString("invalid_payload")]
         InvalidPayload,
         /// <summary>
         /// Indicates that the user being addressed does not exist or is invalid.
         /// The request should not be retried without modification or until the indicated user is set up.
         /// </summary>
-        [EnumMember(Value = "user_not_found")]
+        [EnumString("user_not_found")]
         UserNotFound,
         /// <summary>
         /// Indicates that the channel being addressed does not exist or is invalid.
         /// The request should not be retried without modification or until the indicated channel is set up.
         /// </summary>
-        [EnumMember(Value = "channel_not_found")]
+        [EnumString("channel_not_found")]
         ChannelNotFound,
         /// <summary>
         /// Indicates the specified channel has been archived and is no longer accepting new messages.
         /// </summary>
-        [EnumMember(Value = "channel_is_archived")]
+        [EnumString("channel_is_archived")]
         ChannelIsArchived,
         /// <summary>
         /// Usually means that a team admin has placed some kind of restriction on this avenue of
         /// posting messages and that, at least for now, the request should not be attempted again.
         /// </summary>
-        [EnumMember(Value = "action_prohibited")]
+        [EnumString("action_prohibited")]
         ActionProhibited,
         /// <summary>
         /// is thrown when an incoming webhook attempts to post to the "#general" channel for a team where posting to that channel is
@@ -44,13 +44,13 @@ namespace Narochno.Slack.Entities
         /// 2) the creator of the same incoming webhook is not authorized to post there.
         /// You'll receive this error with a HTTP 403.
         /// </summary>
-        [EnumMember(Value = "posting_to_general_channel_denied")]
+        [EnumString("posting_to_general_channel_denied")]
         PostingToGeneralChannelDenied,
         /// <summary>
         /// Is thrown when an incoming webhook attempts to post a message with greater than 100 attachments.
         /// A message can have a maximum of 100 attachments associated with it.
         /// </summary>
-        [EnumMember(Value = "too_many_attachments")]
+        [EnumString("too_many_attachments")]
         TooManyAttachments,
     }
 }
