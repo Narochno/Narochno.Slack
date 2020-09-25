cde566819dcb8b5087edfdf25b42013e80795d63
Adam Hathcock <adam@hathcock.co.uk>
2020-09-25 08:13:04 +0100

    Remove unused retry properties

--- src/Narochno.Slack/SlackConfig.cs
+++ src/Narochno.Slack/SlackConfig.cs
@@ -13,15 +13,5 @@ namespace Narochno.Slack
         /// The token for non-webhook API methods to use.
         /// </summary>
         public string Token { get; set; }
-
-        /// <summary>
-        /// The number of times requests will be retried 
-        /// </summary>
-        public int RetryAttempts { get; set; } = 2;
-
-        /// <summary>
-        /// The number of retries will be an exponent of this number
-        /// </summary>
-        public int RetryBackoffExponent { get; set; } = 2;
     }
 }
